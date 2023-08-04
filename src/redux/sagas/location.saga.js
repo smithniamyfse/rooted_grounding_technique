d50c2996cd4b9f4344b9489cb3e444d97cc8a334
Amy Smith <smithniamyfse@gmail.com>
2023-08-04 04:28:48 -0500

    udpated location to log the server response and data sent to SQL database

--- src/redux/sagas/location.saga.js
+++ src/redux/sagas/location.saga.js
@@ -3,7 +3,9 @@ import axios from "axios";
 
 function* addLocationData(action) {
     try {
+      console.log(`Location data being sent to the server: ${JSON.stringify(action.payload)}`);
       const response = yield axios.put(`/api/event-entries/${action.payload.eventId}`, action.payload);
+      console.log(`Server response after updating location data: ${JSON.stringify(response.data)}`);
       yield put({ type: "UPDATE_EVENT_ENTRY_SUCCESS", payload: response.data });
     } catch (error) {
       console.log("Error updating location data:", error);
@@ -15,3 +17,23 @@ function* addLocationData(action) {
   }
   
   export default locationSaga;
+
+
+// ** VERSION 1 **
+// import { put, takeLatest } from "redux-saga/effects";
+// import axios from "axios";
+
+// function* addLocationData(action) {
+//     try {
+//       const response = yield axios.put(`/api/event-entries/${action.payload.eventId}`, action.payload);
+//       yield put({ type: "UPDATE_EVENT_ENTRY_SUCCESS", payload: response.data });
+//     } catch (error) {
+//       console.log("Error updating location data:", error);
+//     }
+//   }
+  
+//   function* locationSaga() {
+//     yield takeLatest("ADD_LOCATION_DATA", addLocationData);
+//   }
+  
+//   export default locationSaga;
